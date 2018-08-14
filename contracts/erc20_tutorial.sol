ec67b891084515a675bc5fd037bffc615e05c658
rascallist11 <35939418+rascallist11@users.noreply.github.com>
2018-08-14 12:52:01 +0700

    Update erc20_tutorial.sol
    Yes.

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.18;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'MicroToken' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
-// Total supply: 100000000
+// Deployed to : 0x3AA876933B28B3A96f76dA04BB71A1911a4f6433
+// Symbol      : MTZ
+// Name        : Micro Token
+// Total supply: 10000000000
 // Decimals    : 18
 //
 // Enjoy.
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract MicroToken is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -112,13 +112,13 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
-    function FucksToken() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
+    function Micro Token() public {
+        symbol = "MTZ";
+        name = "Micro Token";
         decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        _totalSupply = 10000000000;
+        balances[0x3AA876933B28B3A96f76dA04BB71A1911a4f6433] = _totalSupply;
+        Transfer(address(0), 0x3AA876933B28B3A96f76dA04BB71A1911a4f6433, _totalSupply);
     }
 
 
